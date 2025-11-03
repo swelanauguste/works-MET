c7cabe2ef994224c1522b1e3ddf4c875a45ffcba
swelanauguste <swelanauguste@gmail.com>
2025-11-03 19:12:42 -0400

    rename the admin url

--- app/cf/urls.py
+++ app/cf/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("met/", admin.site.urls),
     path("", include("reports.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
 ]
