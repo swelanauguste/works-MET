8543de9208cc77460bbaa77f8550c095cf7d0cb7
swelanauguste <swelanauguste@gmail.com>
2025-11-03 13:20:45 -0400

    Format report time display to show hours and minutes

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -15,6 +15,6 @@ python manage.py migrate
 python manage.py makemigrations
 python manage.py createsuperuser --noinput
 python manage.py upload_data
-python manage.py collectstatic --noinput
+# python manage.py collectstatic --noinput
 
 exec "$@"
